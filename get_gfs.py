ba568405f72bc72244a04c40260861b82ea1f120
Jrenaud-Desk <joe.p.renaud@gmail.com>
2024-01-24 14:32:29 -0500

    missed one

--- get_gfs.py
+++ get_gfs.py
@@ -29,7 +29,7 @@ def get_gfs_data(datestr, utc_hour, area, verbose=False):
     url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gens.pl'
     req = requests.get(url)
     if req.error is not None:
-        print "Could not connect! Error code: " % req.error
+        print("Could not connect! Error code: " % req.error)
         sys.exit()
 
     text = req.content.split('</a>')
@@ -93,16 +93,16 @@ def get_gfs_data(datestr, utc_hour, area, verbose=False):
                     '&dir=%2Fgefs.'+day+'%2F'+init+'%2Fpgrb2'
             
                 if verbose:
-                    print ens_url
+                    print(ens_url)
 
                 ens_out = 'ens_' + ens + '.grib2'
                 req = requests.get(ens_url)
 
                 if req.status_code != 200:
-                    print "Could not get ensemble data!"
+                    print("Could not get ensemble data!")
                     break
 
-                print "Saving ensemble member %d" % int(ens)
+                print("Saving ensemble member %d" % int(ens))
                 fid = open(ens_out, 'wb')
                 fid.write(req.content)
                 fid.close()
@@ -137,13 +137,13 @@ def get_gfs_data(datestr, utc_hour, area, verbose=False):
         day+init
 
     if verbose:
-        print ens_main_url
+        print(ens_main_url)
 
     req = requests.get(ens_main_url)
     if req.status_code != 200:
-        print "Could not get ensemble main data!"
+        print("Could not get ensemble main data!")
         sys.exit()
-    print "Saving ensemble main"
+    print("Saving ensemble main")
     fid = open('ens_main.grib2', 'wb')
     fid.write(req.content)
     fid.close()
@@ -167,22 +167,22 @@ def get_gfs_data(datestr, utc_hour, area, verbose=False):
         '&bottomlat='+str(blat)+'&dir=%2Fgfs.'+day+init+'%2Fmaster'
 
     if verbose:
-        print main_url
+        print(main_url)
 
-    print "Saving GFS main"
+    print("Saving GFS main")
     req = requests.get(main_url)
     fid = open('gfs_main.grib2', 'wb')
     fid.write(req.content)
     fid.close()
 
-    print '\n', 'Retrieved data:'
-    print 'GFS main run', good_day, good_init+'Z +', main_step, 'h'
-    print 'Ensemble main run', good_day, good_init+'Z +', main_step, 'h'
-    print 'GFS main and EPS main valid time', datestr, \
-        '%02dZ' % (int(good_init)+int(main_step))
-    print "Ensemble members' run", good_day, good_init+'Z +', ens_step, 'h'
-    print 'Ensemble valid time', \
-        datestr, '%02dZ' % (int(good_init)+int(ens_step))
+    print('\n', 'Retrieved data:')
+    print('GFS main run', good_day, good_init+'Z +', main_step, 'h')
+    print('Ensemble main run', good_day, good_init+'Z +', main_step, 'h')
+    print('GFS main and EPS main valid time', datestr, \
+        '%02dZ' % (int(good_init)+int(main_step)))
+    print("Ensemble members' run", good_day, good_init+'Z +', ens_step, 'h')
+    print('Ensemble valid time', \
+        datestr, '%02dZ' % (int(good_init)+int(ens_step)))
 
 
 if __name__ == '__main__':
@@ -220,192 +220,192 @@ if __name__ == '__main__':
 
 
 
-'''
-rewrite-stuff, not yet completed
+# '''
+# rewrite-stuff, not yet completed
 
-def get_available_days(run_type='main'):
-    '''
-    '''
-    srch = 'gfs'
-    url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl'
+# def get_available_days(run_type='main'):
+#     '''
+#     '''
+#     srch = 'gfs'
+#     url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl'
 
-    if run_type == 'ens':
-        srch = 'gefs'
-        url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gens.pl'
+#     if run_type == 'ens':
+#         srch = 'gefs'
+#         url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gens.pl'
 
-    while True:
-        try:
-            req = requests.get(url)
-            if req.error is not None:
-                print "Could not connect! Error code: " % req.error
-                sys.exit()
-            break
-        except:
-            print 'Request failed, retrying: ' + url
+#     while True:
+#         try:
+#             req = requests.get(url)
+#             if req.error is not None:
+#                 print "Could not connect! Error code: " % req.error
+#                 sys.exit()
+#             break
+#         except:
+#             print 'Request failed, retrying: ' + url
 
-    text = req.content.split('</a>')
-    available_days = []
-    for t in text:
-        if srch in t:
-            available_days.append(t.split(srch+'.')[-1])
+#     text = req.content.split('</a>')
+#     available_days = []
+#     for t in text:
+#         if srch in t:
+#             available_days.append(t.split(srch+'.')[-1])
 
-    available_dates = []
-    for d in available_days:
-        if run_type == 'main':
-            available_dates.append(dt.datetime(int(d[:4]), 
-                                               int(d[4:6]), 
-                                               int(d[6:8]), 
-                                               int(d[8:]), 0, 0))
-        else:
-            available_dates.append(dt.datetime(int(d[:4]), 
-                                               int(d[4:6]), 
-                                               int(d[6:]), 
-                                               0, 0, 0)) # 00 UTC
+#     available_dates = []
+#     for d in available_days:
+#         if run_type == 'main':
+#             available_dates.append(dt.datetime(int(d[:4]), 
+#                                                int(d[4:6]), 
+#                                                int(d[6:8]), 
+#                                                int(d[8:]), 0, 0))
+#         else:
+#             available_dates.append(dt.datetime(int(d[:4]), 
+#                                                int(d[4:6]), 
+#                                                int(d[6:]), 
+#                                                0, 0, 0)) # 00 UTC
 
-    return available_dates
+#     return available_dates
 
 
-def get_ens_inits(date):
+# def get_ens_inits(date):
     
-    base_url = 'http://nomads.ncep.noaa.gov/cgi-bin/' \
-        'filter_gens.pl?dir=%2Fgefs.'
-    srch = 'gefs'
+#     base_url = 'http://nomads.ncep.noaa.gov/cgi-bin/' \
+#         'filter_gens.pl?dir=%2Fgefs.'
+#     srch = 'gefs'
 
-    while True:
-        try:
-            url = base_url + '%4d%02d%02d' % (date.year, 
-                                              date.month, 
-                                              date.day)
+#     while True:
+#         try:
+#             url = base_url + '%4d%02d%02d' % (date.year, 
+#                                               date.month, 
+#                                               date.day)
 
-            req = requests.get(url)
+#             req = requests.get(url)
 
-            if req.error is not None:
-                print "Could not connect! Error code: " % req.error
-                sys.exit()
-            break
-        except:
-            print 'Request failed, retrying: ' + url
+#             if req.error is not None:
+#                 print "Could not connect! Error code: " % req.error
+#                 sys.exit()
+#             break
+#         except:
+#             print 'Request failed, retrying: ' + url
 
-    text = req.content.split('</a>')
-    available_inits = []
-    for t in text:
-        if srch in t:
-            hh = int(t.split(srch+'.')[-1].split('>')[-1])
-            available_inits.append(dt.datetime(date.year,
-                                               date.month,
-                                               date.day,
-                                               hh))
+#     text = req.content.split('</a>')
+#     available_inits = []
+#     for t in text:
+#         if srch in t:
+#             hh = int(t.split(srch+'.')[-1].split('>')[-1])
+#             available_inits.append(dt.datetime(date.year,
+#                                                date.month,
+#                                                date.day,
+#                                                hh))
 
-    return available_inits
+#     return available_inits
 
 
-def download(date, step, area, run_type="main", ens_member=None):
+# def download(date, step, area, run_type="main", ens_member=None):
 
-    blat, tlat, llon, rlon = area
-    day = "%4d%02d%02d" % (date.year, date.month, date.day)
-    init = "%02d" % (date.hour)
+#     blat, tlat, llon, rlon = area
+#     day = "%4d%02d%02d" % (date.year, date.month, date.day)
+#     init = "%02d" % (date.hour)
     
-    step = '%02d' % step
+#     step = '%02d' % step
 
-    url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?' \
-        'file=gfs.t'+init+'z.mastergrb2f'+step+'&lev_1000_mb=on' \
-        '&lev_100_mb=on&lev_10_mb=on&lev_125_mb=on&lev_150_mb=on' \
-        '&lev_175_mb=on&lev_1_mb=on&lev_20_mb=on&lev_225_mb=on' \
-        '&lev_250_mb=on&lev_275_mb=on&lev_2_m_above_ground=on' \
-        '&lev_2_mb=on&lev_300_mb=on&lev_30_mb=on&lev_325_mb=on' \
-        '&lev_350_mb=on&lev_375_mb=on&lev_3_mb=on&lev_400_mb=on' \
-        '&lev_425_mb=on&lev_450_mb=on&lev_475_mb=on&lev_500_mb=on' \
-        '&lev_525_mb=on&lev_550_mb=on&lev_575_mb=on&lev_5_mb=on' \
-        '&lev_600_mb=on&lev_625_mb=on&lev_650_mb=on&lev_675_mb=on' \
-        '&lev_700_mb=on&lev_70_mb=on&lev_725_mb=on&lev_750_mb=on' \
-        '&lev_775_mb=on&lev_7_mb=on&lev_800_mb=on&lev_825_mb=on' \
-        '&lev_850_mb=on&lev_875_mb=on&lev_900_mb=on&lev_925_mb=on' \
-        '&lev_950_mb=on&lev_975_mb=on&lev_surface=on&var_HGT=on' \
-        '&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon='+ \
-        str(llon)+'&rightlon='+str(rlon)+'&toplat='+str(tlat)+ \
-        '&bottomlat='+str(blat)+'&dir=%2Fgfs.'+day+init+'%2Fmaster'
-    out = 'gfs_main.grib2'
+#     url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_hd.pl?' \
+#         'file=gfs.t'+init+'z.mastergrb2f'+step+'&lev_1000_mb=on' \
+#         '&lev_100_mb=on&lev_10_mb=on&lev_125_mb=on&lev_150_mb=on' \
+#         '&lev_175_mb=on&lev_1_mb=on&lev_20_mb=on&lev_225_mb=on' \
+#         '&lev_250_mb=on&lev_275_mb=on&lev_2_m_above_ground=on' \
+#         '&lev_2_mb=on&lev_300_mb=on&lev_30_mb=on&lev_325_mb=on' \
+#         '&lev_350_mb=on&lev_375_mb=on&lev_3_mb=on&lev_400_mb=on' \
+#         '&lev_425_mb=on&lev_450_mb=on&lev_475_mb=on&lev_500_mb=on' \
+#         '&lev_525_mb=on&lev_550_mb=on&lev_575_mb=on&lev_5_mb=on' \
+#         '&lev_600_mb=on&lev_625_mb=on&lev_650_mb=on&lev_675_mb=on' \
+#         '&lev_700_mb=on&lev_70_mb=on&lev_725_mb=on&lev_750_mb=on' \
+#         '&lev_775_mb=on&lev_7_mb=on&lev_800_mb=on&lev_825_mb=on' \
+#         '&lev_850_mb=on&lev_875_mb=on&lev_900_mb=on&lev_925_mb=on' \
+#         '&lev_950_mb=on&lev_975_mb=on&lev_surface=on&var_HGT=on' \
+#         '&var_TMP=on&var_UGRD=on&var_VGRD=on&subregion=&leftlon='+ \
+#         str(llon)+'&rightlon='+str(rlon)+'&toplat='+str(tlat)+ \
+#         '&bottomlat='+str(blat)+'&dir=%2Fgfs.'+day+init+'%2Fmaster'
+#     out = 'gfs_main.grib2'
 
-    if run_type == 'ens':
-        ens_member = '%02d' % ens_member
-        url = 'http://nomads.ncep.noaa.gov/cgi-bin/' \
-            'filter_gens.pl?file=gep'+ens_member+'.t'+init+ \
-            'z.pgrb2f'+step+'&lev_1000_mb=on' \
-            '&lev_100_mb=on&lev_10_mb=on&' \
-            'lev_150_mb=on&lev_200_mb=on&lev_20_mb=on&' \
-            'lev_250_mb=on&lev_2_m_above_ground=on&' \
-            'lev_300_mb=on&lev_30_mb=on&lev_350_mb=on&' \
-            'lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&' \
-            'lev_50_mb=on&lev_550_mb=on&lev_600_mb=on&' \
-            'lev_650_mb=on&lev_700_mb=on&lev_70_mb=on&' \
-            'lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&' \
-            'lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&' \
-            'lev_975_mb=on&lev_surface=on&' \
-            'var_HGT=on&var_TMP=on&var_UGRD=on&var_VGRD=on&' \
-            'subregion=&leftlon='+str(llon)+'&rightlon='+ \
-            str(rlon)+'&toplat='+str(tlat)+'&bottomlat='+str(blat)+ \
-            '&dir=%2Fgefs.'+day+'%2F'+init+'%2Fpgrb2'
+#     if run_type == 'ens':
+#         ens_member = '%02d' % ens_member
+#         url = 'http://nomads.ncep.noaa.gov/cgi-bin/' \
+#             'filter_gens.pl?file=gep'+ens_member+'.t'+init+ \
+#             'z.pgrb2f'+step+'&lev_1000_mb=on' \
+#             '&lev_100_mb=on&lev_10_mb=on&' \
+#             'lev_150_mb=on&lev_200_mb=on&lev_20_mb=on&' \
+#             'lev_250_mb=on&lev_2_m_above_ground=on&' \
+#             'lev_300_mb=on&lev_30_mb=on&lev_350_mb=on&' \
+#             'lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&' \
+#             'lev_50_mb=on&lev_550_mb=on&lev_600_mb=on&' \
+#             'lev_650_mb=on&lev_700_mb=on&lev_70_mb=on&' \
+#             'lev_750_mb=on&lev_800_mb=on&lev_850_mb=on&' \
+#             'lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&' \
+#             'lev_975_mb=on&lev_surface=on&' \
+#             'var_HGT=on&var_TMP=on&var_UGRD=on&var_VGRD=on&' \
+#             'subregion=&leftlon='+str(llon)+'&rightlon='+ \
+#             str(rlon)+'&toplat='+str(tlat)+'&bottomlat='+str(blat)+ \
+#             '&dir=%2Fgefs.'+day+'%2F'+init+'%2Fpgrb2'
 
-        out = 'ens_' + ens_member + '.grib2'
+#         out = 'ens_' + ens_member + '.grib2'
 
-    if run_type == 'ens_main':
-        url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs.pl?file=' \
-            'gfs.t'+init+'z.pgrbf'+step+'.grib2&lev_1000_mb=on&lev_100_mb=on' \
-            '&lev_10_mb=on&lev_150_mb=on&lev_200_mb=on&lev_20_mb=on&lev_250_mb=on' \
-            '&lev_2_m_above_ground=on&lev_300_mb=on&lev_30_mb=on&lev_350_mb=on' \
-            '&lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&lev_50_mb=on' \
-            '&lev_550_mb=on&lev_600_mb=on&lev_650_mb=on&lev_700_mb=on' \
-            '&lev_70_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on' \
-            '&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on' \
-            '&lev_surface=on&var_HGT=on&var_TMP=on&var_UGRD=on&var_VGRD=on' \
-            '&subregion=&leftlon='+str(llon)+'&rightlon='+str(rlon)+ \
-            '&toplat='+str(tlat)+'&bottomlat='+str(blat)+'&dir=%2Fgfs.'+ \
-            day+init
-        out = 'ens_main.grib2'
+#     if run_type == 'ens_main':
+#         url = 'http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs.pl?file=' \
+#             'gfs.t'+init+'z.pgrbf'+step+'.grib2&lev_1000_mb=on&lev_100_mb=on' \
+#             '&lev_10_mb=on&lev_150_mb=on&lev_200_mb=on&lev_20_mb=on&lev_250_mb=on' \
+#             '&lev_2_m_above_ground=on&lev_300_mb=on&lev_30_mb=on&lev_350_mb=on' \
+#             '&lev_400_mb=on&lev_450_mb=on&lev_500_mb=on&lev_50_mb=on' \
+#             '&lev_550_mb=on&lev_600_mb=on&lev_650_mb=on&lev_700_mb=on' \
+#             '&lev_70_mb=on&lev_750_mb=on&lev_800_mb=on&lev_850_mb=on' \
+#             '&lev_900_mb=on&lev_925_mb=on&lev_950_mb=on&lev_975_mb=on' \
+#             '&lev_surface=on&var_HGT=on&var_TMP=on&var_UGRD=on&var_VGRD=on' \
+#             '&subregion=&leftlon='+str(llon)+'&rightlon='+str(rlon)+ \
+#             '&toplat='+str(tlat)+'&bottomlat='+str(blat)+'&dir=%2Fgfs.'+ \
+#             day+init
+#         out = 'ens_main.grib2'
 
-    while True:
-        try:
-            req = requests.get(url)
-        except:
-            print "Connection failed, trying again."
+#     while True:
+#         try:
+#             req = requests.get(url)
+#         except:
+#             print "Connection failed, trying again."
 
-        try:
-            if req.error is None:
-                break
-        except:
-            pass
+#         try:
+#             if req.error is None:
+#                 break
+#         except:
+#             pass
 
-    print "Saving %s" % out
-    fid = open(out, 'wb')
-    fid.write(req.content)
-    fid.close()
-    print "File saved."
+#     print "Saving %s" % out
+#     fid = open(out, 'wb')
+#     fid.write(req.content)
+#     fid.close()
+#     print "File saved."
 
 
-def find_closest_time_and_step(wanted_time, available_times, run_type='main'):
-    '''
-    '''
+# def find_closest_time_and_step(wanted_time, available_times, run_type='main'):
+#     '''
+#     '''
 
-    if run_type == 'main':
-        hour_step = 3
-    else:
-        hour_step = 6
+#     if run_type == 'main':
+#         hour_step = 3
+#     else:
+#         hour_step = 6
 
-    # Take the newest run as baseline
-    closest = available_times[0]
-    closest_diff = closest - wanted_time
+#     # Take the newest run as baseline
+#     closest = available_times[0]
+#     closest_diff = closest - wanted_time
 
-    if closest_diff.total_seconds()/3600. >= hour_step/2:
-        closest = available_times[1]
-        closest_diff = closest - wanted_time
-        for i in xrange(2, len(available_times)):
-            td = available_times[i] - wanted_time
-            if abs(td) <= abs(closest_diff):
-                closest_diff = td
-                closest = available_times[i]
+#     if closest_diff.total_seconds()/3600. >= hour_step/2:
+#         closest = available_times[1]
+#         closest_diff = closest - wanted_time
+#         for i in xrange(2, len(available_times)):
+#             td = available_times[i] - wanted_time
+#             if abs(td) <= abs(closest_diff):
+#                 closest_diff = td
+#                 closest = available_times[i]
 
-    step = int(abs(round((closest_diff.total_seconds()/(3600*hour_step)))))
+#     step = int(abs(round((closest_diff.total_seconds()/(3600*hour_step)))))
         
-    return closest, step
+#     return closest, step
 
-'''
+# '''
